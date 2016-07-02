81004cacdce3a4500b4d9128f245cbd52c681110
jhonny Horacio <jhonny.h.cr@gmail.com>
2016-07-02 14:01:12 -0400

    added search engine

--- server.js
+++ server.js
@@ -64,10 +64,12 @@ app.set('view engine', 'ejs');
 var mainRoutes = require('./routes/main');
 var userRoutes = require('./routes/user');
 var adminRoutes = require('./routes/admin');
+var apiRoutes = require('./api/api');
 
 app.use(mainRoutes);
 app.use(userRoutes);
 app.use(adminRoutes);
+app.use('/api', apiRoutes);
 
 var server = http.createServer(app);
 
